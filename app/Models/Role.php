21a16839bb0d07704868757f5cfe643b9dce038d
Galymzhan K <capomafiosoken@gmail.com>
2020-03-20 13:21:30 +0600

    Models Fix Bugs

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -1,42 +1,19 @@
 <?php
 
-/**
- * Created by Reliese Model.
- */
-
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * Class Role
- * 
- * @property int $id
- * @property string $name
- * 
- * @property Collection|Permission[] $permissions
- * @property Collection|User[] $users
- *
- * @package App\Models
- */
 class Role extends Model
 {
-	protected $table = 'roles';
-	public $timestamps = false;
+    protected $fillable = [
+        'name',
+        'id',
+        ];
 
-	protected $fillable = [
-		'name'
-	];
+    public $timestamps = false;
 
-	public function permissions()
-	{
-		return $this->belongsToMany(Permission::class)
-					->withTimestamps();
-	}
 
-	public function users()
-	{
-		return $this->belongsToMany(User::class, 'user_roles');
-	}
+
 }
+
